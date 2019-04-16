dcb9fb52cc71d462f67bf8d607d6f7577550727e
Mathias Eriksson <mathias542204@gmail.com>
2019-04-16 23:07:48 +0200

    working login page handler func

--- views/login.go
+++ views/login.go
@@ -6,17 +6,6 @@ import (
 )
 
 func Login(w http.ResponseWriter, r *http.Request) {
-    home_page := Page {
-        Title: "Login",
-        Stylesheets: []string {
-            "/public/css/base.css",
-        },
-        Data: map[string] string {
-            "test": "Login Page.",
-        },
-    }
-
-    template, _ := template.ParseFiles("templates/base.html")
-
-    template.Execute(w, home_page)
+  tmpl, _ := template.ParseFiles("public/html/login.html")
+  tmpl.Execute(w, "Login")
 }
