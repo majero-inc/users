9126b8adbf4bb3c3472732f39d5631d02b3e6a2b
Mathias Eriksson <mathias542204@gmail.com>
2019-04-17 21:51:15 +0200

    correction until separation of main and db functionallity

--- main.go
+++ main.go
@@ -2,15 +2,58 @@ package main
 
 import (
 	"bufio"
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"strings"
 
-	_ "github.com/go-sql-driver/mysql"
 	"github.com/majero-inc/users/web"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+type USER struct {
+	username string
+	password string
+	userID   sql.NullInt64
+	groupID  sql.NullInt64
+}
+
+func mysql() {
+	fmt.Print("Go connecting to database\n\n")
+
+	db, err := sql.Open("mysql", "Maedz:test@tcp(127.0.0.1:3306)/demo")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	fmt.Print("Successfully connected to MySQL database\n\n")
+
+	results, err := db.Query("SELECT * FROM USERS")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer results.Close()
+
+	for results.Next() {
+		var user USER
+
+		err = results.Scan(&user.username, &user.password, &user.userID, &user.groupID)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		fmt.Print(user)
+	}
+	fmt.Print("\n\n")
+}
 
 func printCLIHelper() {
 	fmt.Print("Welcome to Majero CLI\nThese are the different tools\n\n")
